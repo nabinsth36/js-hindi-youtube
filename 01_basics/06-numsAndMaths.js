6affd74046898ed05a89ebdfb959e0091caff7b3
Nabin Shrestha <nabinsth@gmail.com>
2024-06-20 09:33:47 +0545

    nums and maths

---
+++ 01_basics/06-numsAndMaths.js
@@ -0,0 +1,29 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+// console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.38966;
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 112400710;
+
+console.log(hundreds.toLocaleString("en-IN"));
+
+console.log(hundreds.toExponential());
+const marks = [21, 45, 19];
+console.log(marks.toString());
+
+// +++++++++++++++++ MATHS ++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 7));
+console.log(Math.max(4, 3, 6, 7));
+console.log(Math.random());
