e66ed92c7a36b67d95d803c63dc533e4e5475d88
nabinsth36 <nabinsth@gmail.com>
2024-06-20 18:27:44 +0545

    nums and maths update

--- 01_basics/06-numsAndMaths.js
+++ 01_basics/06-numsAndMaths.js
@@ -27,3 +27,9 @@ console.log(Math.floor(4.9));
 console.log(Math.min(4, 3, 6, 7));
 console.log(Math.max(4, 3, 6, 7));
 console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)));
