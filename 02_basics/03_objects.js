bf7c43de5c19f6b08bb778e96ee04de41460b8f3
Nabin Shrestha <nabinsth@gmail.com>
2024-06-21 09:35:58 +0545

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// singleton
+
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Hitesh",
+  "full name": "Hitesh Chaudhary",
+  [mySym]: "mykey1",
+  age: 18,
+  location: "Jaipur",
+  email: "hitesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(JsUser.email);
+console.log(JsUser["full name"]);
+console.log(JsUser["email"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com";
+Object.freeze(JsUser);
+JsUser.email = "hitesh@msn.com";
+
+console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JsUser");
+};
+
+console.log(JsUser.greeting);
