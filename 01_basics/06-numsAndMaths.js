b271457f6941095095ae11843d2485122044141b
Nabin Shrestha <nabinsth@gmail.com>
2024-06-21 09:34:48 +0545

    nums and maths

--- 01_basics/06-numsAndMaths.js
+++ 01_basics/06-numsAndMaths.js
@@ -24,6 +24,6 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2));
 console.log(Math.floor(4.9));
-console.log(Math.min(4, 3, 6, 7));
+console.log(xMath.min(4, 3, 6, 7));
 console.log(Math.max(4, 3, 6, 7));
 console.log(Math.random());
