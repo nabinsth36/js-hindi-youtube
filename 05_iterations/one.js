23a8ef156b61c82579b07de4f52606bbe758d0f5
Nabin Shrestha <nabinsth@gmail.com>
2024-06-23 23:47:25 +0545

    for loop and break and continue keywords

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -24,6 +24,14 @@ for (let i = 0; i < myArray.length; i++) {
 
 // break and continue
 
+// for (let index = 1; index <= 20; index++) {
+//   if (index == 5) {
+//     console.log(`Detected 5`);
+//     break;
+//   }
+//   console.log(`Value of i is ${index}`);
+// }
+
 for (let index = 1; index <= 20; index++) {
   if (index == 5) {
     console.log(`Detected 5`);
