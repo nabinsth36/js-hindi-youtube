632cc21023d28b005f657f8219c0cff7df36fd1d
Nabin Shrestha <nabinsth@gmail.com>
2024-06-23 23:49:12 +0545

    for of loop and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["", "", ""];
+
+// [{}, {}, {}];
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  console.log(num);
+}
+
+//////
+
+const greetings = "Hello World";
+for (const greet of greetings) {
+  console.log(`Each char is ${greet}`);
+}
+
+// Maps
+const map = new Map();
+map.set("In", "India");
+map.set("NP", "Nepal");
+map.set("JP", "Japan");
+map.set("In", "India");
+
+console.log(map);
+
+for (const [key, value] of map) {
+  console.log(`${key} => ${value}`);
+}
+
+const myObject = {
+  game1: "NFS",
+  game2: "GTA",
+  game3: "PUBG",
+  game4: "Fortnite",
+};
+
+// for (const [key, value] of myObject) {
+//   console.log(`${key} => ${value}`);
+// }
